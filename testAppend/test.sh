cd87e5af7e431b2a505d4bc7e00b705d0067900d
Your Name <you@example.com>
2018-11-30 12:52:33 +0100

    Nowe poprawki do tego hmmmm

---
+++ testAppend/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Display WWPNs on a Linux server.
+# Add my commands
+
+[ -d "/sys/class/fc_host" ] && cat /sys/class/fc_host/*/port_name | sed 's/^0x//; s/\(.\{2\}\)/&:/g; s/:$//'
+
