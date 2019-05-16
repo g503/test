66367a48fee9ca635a4df0bb25f6694165f0f6c7
Szczurek <grsc@MobicaPL.local>
2019-05-16 08:09:48 +0200

    Zmiana w pliku 1

--- testAppend/test.sh
+++ testAppend/test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Display WWPNs on a Linux server.
-<<<<<<< HEAD
+
 # Add my commands
-=======
->>>>>>> 7105b45a8e96a473acc38de38a30051e7dea8005
 
 [ -d "/sys/class/fc_host" ] && cat /sys/class/fc_host/*/port_name | sed 's/^0x//; s/\(.\{2\}\)/&:/g; s/:$//'
 
